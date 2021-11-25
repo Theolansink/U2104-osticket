a46da5d7cc92dfa418741ef47e82b433a8d229db
Theolansink <34719919+Theolansink@users.noreply.github.com>
2021-11-25 11:25:31 +0100

    Add files via upload

--- webserver-check.ps1
+++ webserver-check.ps1
@@ -4,9 +4,9 @@
 #######################################
 
 echo -e "\n\n"
-echo "    This script checks if the file $(tput setaf 2)/var/www/html/osTicket/upload/index.php $(tput setaf 0) exists"
+echo "    This script checks if the file $(tput setaf 2)/var/www/html/osTicket/upload/index.php $(tput setaf 7) exists"
 echo "    If that file exists osTicket is probably correctly installed"
-echo -e "    It further checks if the webserver $(tput setaf 2)Apache $(tput setaf 0)is running \n\n"
+echo -e "    It further checks if the webserver $(tput setaf 2)Apache $(tput setaf 7)is running \n\n"
 
 #####################################################################
 # first check if file /var/www/html/osTicket/upload/index.php exsists
@@ -14,9 +14,9 @@ echo -e "    It further checks if the webserver $(tput setaf 2)Apache $(tput set
 
 if [[ -f /var/www/html/osTicket/upload/index.php ]]
 then  
-  echo "   $(tput setaf 2)OK $(tput setaf 0)The file /var/www/html/osTicket/upload/index.php exists"
+  echo "   $(tput setaf 2)OK $(tput setaf 7)The file /var/www/html/osTicket/upload/index.php exists"
 else
-  echo "   $(tput setaf 1)FALSE $(tput setaf 0)The file /var/www/html/osTicket/upload/index.php does not exist"
+  echo "   $(tput setaf 1)FALSE $(tput setaf 7)The file /var/www/html/osTicket/upload/index.php does not exist"
   echo "          Probably osTicket is not correctly installed"
 fi
 
@@ -26,7 +26,7 @@ fi
 
 if pgrep apache > /dev/null
 then  
-  echo -e "   $(tput setaf 2)OK $(tput setaf 0)The webserver Apache is running \n\n\n"
+  echo -e "   $(tput setaf 2)OK $(tput setaf 7)The webserver Apache is running \n\n\n"
 else
-  echo "   $(tput setaf 1)FALSE $(tput setaf 0)The webserver Apache is not running \n\n\n"
+  echo "   $(tput setaf 1)FALSE $(tput setaf 7)The webserver Apache is not running \n\n\n"
 fi
