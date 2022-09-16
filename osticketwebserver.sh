3b7b7eae439e9cb6c16922cbdb2965895959be5a
Theolansink <34719919+Theolansink@users.noreply.github.com>
2022-09-16 08:00:38 +0200

    Update yum changed to wget

--- osticketwebserver.sh
+++ osticketwebserver.sh
@@ -6,7 +6,7 @@ sudo apt install php libapache2-mod-php php-{fpm,pear,imap,apcu,intl,cgi,common,
 sudo apt-get install curl
 sudo service apache2 start
 sudo systemctl enable apache2
-sudo yum https://github.com/osTicket/osTicket/releases/download/v1.15.4/osTicket-v1.15.4.zip
+sudo wget https://github.com/osTicket/osTicket/releases/download/v1.15.4/osTicket-v1.15.4.zip
 sudo apt-get install unzip
 sudo unzip osTicket-v*.zip -d osTicket
 sudo mv osTicket /var/www/html
